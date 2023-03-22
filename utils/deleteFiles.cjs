2ddc9fa673ed19faf3828ab7058b605ff7db6d45
TejashriSJ <tejashri.j.23.2@mountblue.tech>
2023-03-22 09:49:50 +0530

    Add deleteFiles file
    This will delete the files specified by the user

---
+++ utils/deleteFiles.cjs
@@ -0,0 +1,20 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+function deleteFiles(dirName, files) {
+  return new Promise((resolve, reject) => {
+    let delteFilesPromises = files.map((file) => {
+      let filePath = path.join(__dirname, "userCreatedFiles/", dirName, file);
+      return fs.unlink(filePath);
+    });
+    Promise.allSettled(delteFilesPromises)
+      .then((settledPromises) => {
+        resolve(settledPromises);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+}
+
+module.exports = deleteFiles;
