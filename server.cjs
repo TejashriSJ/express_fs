4a7124fa09dffbf38d6ff5b05307fe54299d75d0
TejashriSJ <tejashri.j.23.2@mountblue.tech>
2023-03-23 07:21:16 +0530

    Update server file
    This will add separate routes for post and delete

--- server.cjs
+++ server.cjs
@@ -1,21 +1,26 @@
 const express = require("express");
-const router = require("./routes/index.cjs");
+const routerPostRequest = require("./routes/postRequest.cjs");
+const routerDeleteRequest = require("./routes/deleteRequest.cjs");
 const createError = require("http-errors");
 
 const server = express();
 const PORT = process.env.PORT || 3000;
 
 server.use(express.json());
-server.use("/", router);
+server.use(express.urlencoded({ extended: false }));
+
+server.post("/", routerPostRequest);
+server.delete("/", routerDeleteRequest);
 
 //catch 404 and forward to error handler
 server.use((req, res, next) => {
   next(createError(404));
 });
-//Handles generic errors
+
 server.use((err, req, res, next) => {
   res.status(err.status || 500);
-  res.json({ Status: "Error" });
+
+  res.json(err.messageObj || { Status: "Error" });
 });
 
 server.listen(PORT, () => {
